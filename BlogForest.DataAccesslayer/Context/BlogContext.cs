8df0be4230f0b84e9967397a3afbf10ab9fd0500
Yahyaygmr <yahyaygmr@gmail.com>
2024-05-21 21:01:58 +0300

    committed

---
+++ BlogForest.DataAccesslayer/Context/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using BlogForest.Entitylayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogForest.DataAccesslayer.Context
+{
+    public class BlogContext : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-4QIIH5S;database=ForestBlogDb;integrated security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
